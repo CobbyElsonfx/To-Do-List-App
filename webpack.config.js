6b0c9a8f534597846e6ad0f8bd01b58c5e798bde
MR. ANDOH FRANCIS <andohfrancis9187@gmail.com>
2023-07-14 16:50:40 +0000

    added list listing structure

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
@@ -14,9 +13,10 @@ module.exports = {
     }),
   ],
   output: {
-    filename: 'main.js',
+    filename: '[name]. bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+    assetModuleFilename: 'images/[name][ext]',
   },
   module: {
     rules: [
@@ -24,6 +24,11 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
         exclude: /node_modules/,
+      },
+
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)/i,
+        type: 'asset/resource',
       },
     ],
   },
