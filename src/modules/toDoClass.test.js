f07511ef3be83ae50e84d97534958c6f448aa379
MR. ANDOH FRANCIS <andohfrancis9187@gmail.com>
2023-07-26 09:32:55 +0000

    fixed linters error

--- src/modules/toDoClass.test.js
+++ src/modules/toDoClass.test.js
@@ -1,7 +1,7 @@
 import ToDoClass from './toDoClass.js';
 
-// Helper function to mock localStorage
-const localStorageMock = function () {
+// Function to mock localStorage
+const localStorageMock = (() => {
   let store = {};
 
   return {
@@ -9,7 +9,7 @@ const localStorageMock = function () {
     setItem: (key, value) => (store[key] = value.toString()),
     clear: () => (store = {}),
   };
-}();
+})();
 
 Object.defineProperty(window, 'localStorage', { value: localStorageMock });
 
@@ -17,20 +17,23 @@ describe('ToDoClass', () => {
   let todo;
   let listElementMock;
 
-  beforeEach(() => {
-    // Clear localStorage and create a new instance of ToDoClass before each test
+  beforeEach(() => { 
     localStorageMock.clear();
     todo = new ToDoClass();
-
-    // Create a mock list element and set it as the return value for document.querySelector
     listElementMock = document.createElement('ul');
     listElementMock.setAttribute('id', 'list');
     document.querySelector = jest.fn().mockReturnValue(listElementMock);
   });
 
-  // add new task
   test('addActivity should add a new activity', () => {
+    const appendChildMock = jest.spyOn(listElementMock, 'appendChild');
+
     todo.addActivity('Test Activity');
+
+    // Check if the new activity was added to the DOM
+
+    expect(appendChildMock).toHaveBeenCalled();
+    expect(listElementMock.children.length).toBe(1);
     expect(todo.listArray.length).toBe(1);
     expect(todo.listArray[0].description).toBe('Test Activity');
     expect(todo.listArray[0]).toEqual({
@@ -40,12 +43,13 @@ describe('ToDoClass', () => {
     });
   });
 
-  //Remove or delete activity 
   test('removeActivity should remove an activity', () => {
     todo.addActivity('Activity 1');
     todo.addActivity('Activity 2');
+
     todo.removeActivity(1);
     expect(todo.listArray.length).toBe(1);
     expect(todo.listArray[0].description).toBe('Activity 2');
+    expect(listElementMock.children.length).toBe(1); // Check the length of the <ul> element
   });
 });
